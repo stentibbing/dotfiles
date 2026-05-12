457654a342af1365b268d0877286a1b1f7b86e51
Sten Tibbing <sten.tibbing@gmail.com>
2026-05-12 19:31:10 +0300

    gnome terminal palette

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,10 +22,7 @@ vim.pack.add({
 	"https://github.com/folke/flash.nvim",
 	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/nvim-lualine/lualine.nvim",
-	"https://github.com/NopAngel/nimmy.vim",
-	"https://github.com/marcos-venicius/zenburned",
-	"https://github.com/fdemb/dusk.nvim",
-	"https://github.com/eggfriedrice24/eggfriedrice.nvim"
+	"https://github.com/srcery-colors/srcery-vim"
 })
 
 require("nvim-treesitter").setup({
@@ -108,6 +105,61 @@ require("codecompanion").setup({
 	},
 })
 
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = ' ', right = ' '},
+    section_separators = { left = ' ', right = ' '},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        'WinEnter',
+        'BufEnter',
+        'BufWritePost',
+        'SessionLoadPost',
+        'FileChangedShellPost',
+        'VimResized',
+        'Filetype',
+        'CursorMoved',
+        'CursorMovedI',
+        'ModeChanged',
+      },
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
 require("flash").setup({
 	modes = {
 		char = {
@@ -170,7 +222,8 @@ vim.diagnostic.config({
 })
 
 --- Options ---
-vim.cmd.colorscheme("nimmy-hard")
+vim.cmd.colorscheme("srcery")
+vim.opt.background = "dark"
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
--- .zshrc
+++ .zshrc
@@ -10,21 +10,14 @@ unsetopt beep
 bindkey -v
 bindkey '^A' autosuggest-accept
 
-# export LANG="en_US.UTF-8"
-# export LC_ALL="en_US.UTF-8"
 export EDITOR="nvim"
 export VISUAL="nvim"
-export XDG_OPEN="brave"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
 export MANPAGER="nvim +Man!"
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/config.toml"
 
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/go/bin:$PATH"
-export PATH="$HOME/.npm/global/bin:$PATH"
-export PATH="$HOME/.local/share/pnpm:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 alias l='ls -lah --color=auto'
 alias y='yazi'
@@ -35,14 +28,14 @@ autoload -Uz compinit
 compinit
 
 # syntax highlighting
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # autosuggestions
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 eval "$(zoxide init zsh)"
 eval "$(starship init zsh)"
 
 export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
